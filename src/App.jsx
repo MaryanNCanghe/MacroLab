9c84f725453d429a6798c012b5325596214215d9
Kiki <marianecanhe@gmail.com>
2025-04-13 21:07:44 +0200

    changes of the home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'; // Importe para estilos adicionais se necessário
 
 // Importe da imagem de background
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Macro-Lab/Home', // Base URL for GitHub Pages
+  base: '/Macro-Lab/', // Base URL for GitHub Pages
   build: {
     outDir: 'dist',
   },
